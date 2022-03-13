529de564c7f68beeb91e7bbb0fc877912b5f8162
Billal Asif <asif.billal@outlook.com>
2022-03-13 18:17:59 +0000

    Added header

---
+++ src/Booking.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const BookingAppointment = () => {
+
+    return (
+        <h1 className="header">Book an appointment</h1>
+    )
+
+}
+
+export default BookingAppointment;
