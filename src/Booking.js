001100b5857f55756424e0d7951d07d4e4f4268e
Billal Asif <asif.billal@outlook.com>
2022-03-14 13:07:04 +0000

    Header update

--- src/Booking.js
+++ src/Booking.js
@@ -4,7 +4,18 @@ import "./App.css";
 const BookingAppointment = () => {
 
     return (
-        <h1 className="header">Book an appointment</h1>
+        <>
+            <div className="tracking-wide grid mix-blend-multiply bg-blue-400 justify-items-center">
+
+                <h1 className="my-5 font-serif tracking-wide text-2xl font-semibold te-yellow-500 shadow-yellow">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="fill-yellow-500 inline-block h-7 w-10 mb-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                    </svg>
+                    Vet Booking System
+                </h1>
+
+            </div>
+        </>
     )
 
 }
