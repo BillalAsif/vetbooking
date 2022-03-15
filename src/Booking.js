db80504af5b23022e52244a52a8c99941b4bfd3d
Billal Asif <asif.billal@outlook.com>
2022-03-15 20:50:09 +0000

    forms added

--- src/Booking.js
+++ src/Booking.js
@@ -9,17 +9,18 @@ const Book = () => {
                     <h1 className="text-center py-5 text-stone-50 font-medium text-lg">Book an appointment</h1>
                 </div>
                 <div className="border-cyan-500 border-2">
-                    <div className="grid-rows-3">
-                        <div>
-                            <input type="text" />
-                        </div>
-                        <div>
 
-                        </div>
-                        <div>
-
-                        </div>
+                    <div className="grid grid-rows-2 grid-flow-col gap-4">
+                        <div><p className="inline-block">Name: </p><input className="float-right" type="text" /></div>
+                        <div><p className="inline-block">Date of birth: </p><input className="float-right" type="date" /></div>
+                        <div><p className="inline-block">Appointment Date: </p><input className="float-right" type="date" /></div>
+                        <div><p className="inline-block">Appointment Time: </p><input className="float-right" type="time" /></div>
                     </div>
+
+                    <div className="block">
+                        <p className="inline-block">Notes: </p><textarea className="inline-block" type="textarea" />
+                    </div>
+
                 </div>
             </div>
         </>
