5815e72e0cbbbd009c13ff9aeeee8f206f802f51
Billal Asif <asif.billal@outlook.com>
2022-03-15 10:45:19 +0000

    appointment div started

--- src/Booking.js
+++ src/Booking.js
@@ -1,3 +1,30 @@
 import React, { useEffect, useState } from "react";
 
+const Book = () => {
+    return (
 
+        <>
+            <div className="container mx-auto px-20 mt-4">
+                <div className="bg-blue-500">
+                    <h1 className="text-center py-5 text-stone-50 font-medium text-lg">Book an appointment</h1>
+                </div>
+                <div className="border-cyan-500 border-2">
+                    <div className="grid-rows-3">
+                        <div>
+                            <input type="text" />
+                        </div>
+                        <div>
+
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default Book;
