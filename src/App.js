7385576048a3491c41f066352b801feae6221115
Billal Asif <asif.billal@outlook.com>
2022-03-30 16:56:18 +0100

    useEffect added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import Header from "./Header";
 import BookAppoitment from "./Booking";
 import Search from "./Search";
+import {useEffect, useState} from 'react';
+
 
 function App() {
+
+  let searchBar = document.getElementById('search-bar');
+
+  useEffect( () => {
+    
+    return () => {
+      console.log("change dected")
+    }
+
+  }, [searchBar])
+
   return (
     <>
       <Header />
