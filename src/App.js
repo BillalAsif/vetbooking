7c4a795234c5da8446d1df6bcb4dcb2b990a76c2
Billal Asif <asif.billal@outlook.com>
2022-04-07 23:11:40 +0100

    Attempting useEffect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import Header from "./Header";
 import BookAppoitment from "./Booking";
 import {Search, callSearch} from "./Search";
-import {useEffect, useState} from 'react';
 
 
 function App() {
-
-  useEffect( () => {
-    
-    return () => {
-      console.log("change dected")
-    }
-
-  }, [callSearch])
 
   return (
     <>
